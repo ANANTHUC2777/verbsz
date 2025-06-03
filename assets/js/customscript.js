05c20430e77efb883fc207bc6026ed2cbb8ae3b3
Ananthu <ananthu.c.666@gmail.com>
2025-06-03 09:17:44 +0530

    three pages resp commit

--- assets/js/customscript.js
+++ assets/js/customscript.js
@@ -5,3 +5,29 @@ navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("change");
   navMenu.classList.toggle("active");
 });
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {
+      $("#return-to-top").fadeIn(200);
+    } else {
+      $("#return-to-top").fadeOut(200);
+    }
+  });
+  $("#return-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
+  $(window).on("scroll", function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll > 20) {
+      $("header").addClass("effect");
+    } else {
+      $("header").removeClass("effect");
+    }
+  });
+});
+new WOW().init();
